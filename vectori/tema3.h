3050a0cde1eb528fe49ea1a6351eb7d491a8e64d
victorcustrin-hub <victorcustrin@gmail.com>
2026-07-04 14:43:38 +0300

    finish 5->10 ex

--- vectori/tema3.h
+++ vectori/tema3.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+//185
+
 
 //1
 void sortatPrinBubbleSortDescrescator(int v[], int d) {
@@ -208,4 +210,169 @@ void solutie511() {
 	afisare(v, d);
 
 }
+//
 
+//6
+void solutie512() {
+	int v[100] = { 13, 1, 10, 15, 3, 7, 11 };
+	int d = 7;
+	int max = -1;
+	int ct = 0;
+	for (int i = 0;i < d;i++) {
+		if (v[i] > max) {
+			max = v[i];
+			ct++;
+		}
+	}
+
+	sortatCrescatorPartial(v, 0, ct);
+	sortatDescrescatorPartial(v, ct+1, d - 1);
+	afisare(v,d);
+
+}
+//
+
+//7
+void solutie180() {
+	int v[100] = { 10, 20, 15, 7, 12 };
+	int d = 5;
+	int ct = 0;
+	int ct2 = 0;
+
+	for (int i = 1; i < d; i++) {
+		if (v[i] < v[ct]) {
+			ct = i;
+		}
+		if (v[i] > v[ct2]) {
+			ct2 = i;
+		}
+	}
+
+	int st = ct;
+	int dr = ct2;
+
+	if (st > dr) {
+		int aux = st;
+		st = dr;
+		dr = aux;
+	}
+
+	sortatCrescatorPartial(v, st, dr);
+	afisare(v, d);
+}
+//
+
+
+//8
+bool patratPerfect(int n) {
+
+	return sqrt(n) == (int)sqrt(n);
+
+}
+
+void bubbleSortPatratPerfectCrescator(int v[],int d) {
+
+	for (int i = 0;i < d;i++) {
+		if (patratPerfect(v[i])) {
+			for (int j = i + 1; j < d; j++) {
+				if (patratPerfect(v[j]) && v[i] > v[j]) {
+					int aux = v[i];
+					v[i] = v[j];
+					v[j] = aux;
+				}
+			}
+		}
+		
+	}
+}
+
+void solutie183() {
+	int v[100] = {9, 15, 16, 4, 5, 1, 7, 9};
+	int d = 8;
+
+	bubbleSortPatratPerfectCrescator(v, d);
+	afisare(v, d);
+}
+//
+
+//9
+int div2(int n) {
+	int s = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			s = s + i;
+		}
+	}
+	return s;
+}
+
+void BubbleSortCrescatorDivizori(int v[], int d) {
+	bool sortat = true;
+	do {
+		sortat = true;
+
+		for (int i = 0;i < d - 1;i++) {
+
+			if (div2(v[i]) > div2(v[i + 1]) || (div2(v[i]) == div2(v[i + 1]) && v[i] > v[i + 1])) {
+				int aux = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = aux;
+				sortat = false;
+			}
+
+		}
+
+	} while (sortat == false);
+}
+
+void solutie515() {
+	int v[100] = { 24, 46, 11, 36, 48, 35, 27, 28, 49, 6 };
+	int d = 10;
+
+	BubbleSortCrescatorDivizori(v, d);
+	afisare(v, d);
+}
+//
+
+//10
+int primaCif(int n) {
+
+	while (n > 9) {
+		n = n / 10;
+	}
+	return n;
+}
+
+void BubbleSortCrescator(int v[], int d) {
+	bool sortat = true;
+	do {
+		sortat = true;
+		for (int i = 0; i < d - 1; i++) {
+			if (v[i] > v[i + 1]) {
+				int aux = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = aux;
+				sortat = false;
+			}
+		}
+	} while (sortat == false);
+}
+
+void solutie185() {
+	int v[100] = { 100, 312, 276, 985, 5021 };
+	int d = 5;
+	int cif[100];
+
+	for (int i = 0; i < d; i++) {
+		cif[i] = primaCif(v[i]);
+	}
+	BubbleSortCrescator(cif, d);
+
+
+	for (int i = 0; i < d; i++) {
+		cout << cif[i];
+	}
+
+}
+//
--- vectori/vectori.cpp
+++ vectori/vectori.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-	solutie511();
+	solutie185();
 
 
 }
