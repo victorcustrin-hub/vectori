66567b268d1485ec47294094d3d199a606df42c5
victorcustrin-hub <victorcustrin@gmail.com>
2026-08-05 19:59:51 +0300

    finish pbinfo 518+solutii

--- vectori/secvente.h
+++ vectori/secvente.h
@@ -3,14 +3,6 @@
 #include <cmath>
 using namespace std;
 
-void afisare(int v[], int start, int end) {
-
-    for (int i = start;i <= end;i++) {
-        cout << v[i] << " ";
-    }
-
-}
-
 // int v[100] = { 2,4,5,7,11,19,24,21,31,41 },  d=10
 
 //  i<dim      v[i]%2==1     j    j+i<dim && v[j+1]%2==1      j      smax           dmax     i
@@ -25,7 +17,6 @@ void afisare(int v[], int start, int end) {
 //   -             -         -       9<10 da, 41=impar        10      -               -      -
 //   -             -         -       10<10 nu                 -       2 (if==false)   5      10
 //  10 < 10 nu   
-
 void ceaMaiLungaSecventaDeNumereImpare(int v[], int dim) {
     int smax, dmax;
     smax = 1, dmax = 0;
@@ -43,12 +34,19 @@ void ceaMaiLungaSecventaDeNumereImpare(int v[], int dim) {
             i = j;
         }
     }
+    for (int i = smax;i <= dmax;i++) {
+        cout << v[i] << " ";
+    }
+}
 
-    afisare(v, smax, dmax);
+void solutie1() {
+    int v[100] = { 2,4,5,7,11,19,24,21,31,41 };
+    int d = 10;
+    
+    ceaMaiLungaSecventaDeNumereImpare(v, d);
 }
 
 // vector cu n elemente,Calculați suma elementelor din secvența determinată de primul și ultimul element impar.
-
 void secventaPrimulSiUltimulNumarImpar(int v[], int d) {
     int primul = -1;
     int ultimul = -1;
@@ -61,15 +59,51 @@ void secventaPrimulSiUltimulNumarImpar(int v[], int d) {
             ultimul = i;
         }
     }
-    
     if (primul != -1) {
         int s = 0;
         for (int i = primul;i <= ultimul;i++) {
             s = s + v[i];
         }
-        cout << "Suma dintre " << primul << ", primul numar impar din sir, cu " << ultimul << ", ultimul numar impar din sir, este " << s << endl;
+        cout << "Suma dintre " << v[primul] << ", primul nr impar din sir cu " << v[ultimul] << ", ultimul nr impar din sir, este " << s << endl;
     }
     else {
         cout << "Nu exista elemente impare in vector" << endl;
     }
+}
+
+void solutie2() {
+    int v[100] = { 12,10,15,6,7,10,19,14 };
+    int d = 8;
+    secventaPrimulSiUltimulNumarImpar(v, d);
+}
+
+//Determinați cea mai lungă secvență de elemente nule din vector.
+//Dacă în vector există mai multe secvențe de elemente nule de lungime maximă se va determina cea mai din stânga.
+void secventaElementeNuleMaxima(int v[], int d) {
+    int smax = 0;
+    int dmax = -1;
+
+    for (int i = 0;i < d;i++) {
+        if (v[i] == 0) {
+            int j = i;
+            while (j + 1 < d && v[j + 1] == 0) {
+                j++;
+            }
+
+            if (j - i > dmax - smax) {
+                smax = i + 1;
+                dmax = j + 1;;
+            }
+            i = j;
+        }
+
+    }
+    cout << smax << " " << dmax;
+}
+
+void solutie3() {
+    int v[100] = { 0,1,0,0,0,3,0,0,0,2 };
+    int d = 10;
+
+    secventaElementeNuleMaxima(v, d);
 }
--- vectori/vectori.cpp
+++ vectori/vectori.cpp
@@ -3,7 +3,5 @@
 using namespace std;
 
 int main() {
-    int v[100] = { 12, 10, 15, 6, 7, 10, 19, 14 };
-    int d = 8;
-    secventaPrimulSiUltimulNumarImpar(v, d);
+    solutie3();
 }
