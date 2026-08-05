5ac477168c8e9cb92850cff72a40d163b5180363
victorcustrin-hub <victorcustrin@gmail.com>
2026-08-05 19:09:00 +0300

    finish frecvanta nr pare

--- vectori/secvente.h
+++ vectori/secvente.h
@@ -4,6 +4,17 @@
 using namespace std;
 
 
+
+// int v[100] = { 2,4,5,7,11,19,24,21,31,41,19 },  d=12
+
+//  i<dim      v[i]%2==1     j    j+i<dim && v[j+1]%2==1      j   i
+//  0<12 da      2 nu        -           -                    -   1
+//  1<12 da      4 nu        -           -                    -   2
+//  2<12 da      5 da        2       3<12 da, 7=impar         3   -  
+//   -             -         -       4<12 da, 11=impar        4   -
+//   -             -         -       5<12 da, 19=impar        5   -
+//   -             -         -       6<12 da, 20=impar                    
+
 void ceaMaiLungaSevcentaDeNumerePare(int v[], int dim) {
     int smax, dmax;
     smax = 1, dmax = 0;
--- vectori/vectori.cpp
+++ vectori/vectori.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int v[100] = { 2,4,5,6,7,11,19,24,21,31,41,19 };
+    int v[100] = { 2,4,5,7,11,19,24,21,31,41 };
     int d = 12;
 
     ceaMaiLungaSevcentaDeNumerePare(v,d);
